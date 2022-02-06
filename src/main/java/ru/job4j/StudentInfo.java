6951741ad54e0544492d7fc285416444e6de3770
Aleksandr Kochkurkin <info@debitop.com>
2022-02-06 23:12:01 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Aleksandr Kochkurkin");
+        System.out.println("12.06.1981");
+    }
+}
