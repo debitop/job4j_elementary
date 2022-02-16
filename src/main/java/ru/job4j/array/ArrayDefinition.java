aa7a2d996ca9fd40ac4f3e4881438375f8a3ebdc
Aleksandr Kochkurkin <info@debitop.com>
2022-02-16 09:07:28 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,8 +5,17 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Aleksandr";
+        names[1] = "Ivan";
+        names[2] = "Oleg";
+        names[3] = "Mihail";
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
