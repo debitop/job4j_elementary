f73c3566c215b2c30be77aac0f69d9534334c0b6
Aleksandr <89263693878@mail.ru>
2024-08-24 10:21:55 +0300

    GPT. Нейросети спешат на помощь. [#505156 #510379]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -1,22 +1,38 @@
 package ru.job4j.calculator;
 
 public class Fit {
-    public static double manWeight(short height) {
-        double result = (height - 100) * 1.15;
-        return result;
+
+    private static final int MALE_BASE_HEIGHT = 100;
+    private static final int FEMALE_BASE_HEIGHT = 110;
+    private static final double COEFFICIENT = 1.15;
+
+    public static double calculateIdealWeight(short height, int baseHeight) {
+        return (height - baseHeight) * COEFFICIENT;
     }
 
-    public static double womanWeight(short height) {
-        double result = (height - 110) * 1.15;
-        return result;
+    public static double manWeight(short heightMan) {
+        return calculateIdealWeight(heightMan, MALE_BASE_HEIGHT);
+    }
+
+    public static double womanWeight(short heightWoman) {
+        return calculateIdealWeight(heightWoman, FEMALE_BASE_HEIGHT);
     }
 
     public static void main(String[] args) {
-        short height = 187;
-        double man = Fit.manWeight(height);
-        double woman = Fit.womanWeight(height);
+        short heightMan = 187;
+        short heightWoman = 170;
 
-        System.out.println("Man 187 is " + man);
-        System.out.println("Woman 187 is " + woman);
+        if (heightMan <= 0 || heightWoman <= 0) {
+            System.out.println("Height must be greater than 0.");
+            return;
+        }
+
+        double man = manWeight(heightMan);
+        System.out.println("Ideal weight for a man with height 187 cm is " + man + " kg.");
+
+        double woman = womanWeight(heightWoman);
+        System.out.println("Ideal weight for a woman with height 170 cm is " + woman + " kg.");
     }
 }
+
+
