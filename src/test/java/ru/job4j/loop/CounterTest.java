6676bd2cc1105f1b25cfdfa5b177f674db895bb0
Aleksandr <89263693878@mail.ru>
2024-08-26 09:59:13 +0300

    5.0.1. Сумма чисел. [#226416 #264308]

---
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,53 @@
+package ru.job4j.loop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CounterTest {
+
+    @Test
+    void when0To10Then55() {
+        int start = 0;
+        int finish = 10;
+        int expected = 55;
+        int output = Counter.sum(start, finish);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void when0To5Then15() {
+        int start = 0;
+        int finish = 5;
+        int expected = 15;
+        int output = Counter.sum(start, finish);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void when3To8Then33() {
+        int start = 3;
+        int finish = 8;
+        int expected = 33;
+        int output = Counter.sum(start, finish);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void when1To1Then1() {
+        int start = 1;
+        int finish = 1;
+        int expected = 1;
+        int output = Counter.sum(start, finish);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void when3To1Then0() {
+        int start = 3;
+        int finish = 1;
+        int expected = 0;
+        int output = Counter.sum(start, finish);
+        assertThat(output).isEqualTo(expected);
+    }
+}
